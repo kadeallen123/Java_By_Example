0e2c8f81a01828354c36a6b992630c2acb74762f
kadeallen123 <38875538+kadeallen123@users.noreply.github.com>
2018-11-27 16:00:33 +1300

    Comments, Util Import

--- src/java_by_example/Main.java
+++ src/java_by_example/Main.java
@@ -1,21 +1,18 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+    This programs displays to the user example Java programs that are categorised
+    Author: Kade Allen
+    Date of creation:   27/11/18
+*/
+
 package java_by_example;
 
-/**
- *
- * @author Kade Allen
- */
+import java.util.*;
+
 public class Main {
 
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+       
     }
     
 }
