c94a296d525f59116d9836ca3ae058e1dd50f2d9
kadeallen123 <38875538+kadeallen123@users.noreply.github.com>
2018-11-27 16:06:37 +1300

    Added welcome and instructions

--- src/java_by_example/Main.java
+++ src/java_by_example/Main.java
@@ -13,7 +13,17 @@ public class Main {
     static Scanner input = new Scanner(System.in);
     
     public static void main(String[] args) {
-       
+       // Welcome statement
+        System.out.println("Welcome to Java By Examples\nPress 'Enter' to continue");
+        input.nextLine();
+        System.out.println("Enter the correpsonding number to view example category:\n"
+                + "\t'1' to view Simple Programs\n"
+                + "\t'2' to view Loops / Iterations\n"
+                + "\t'3' to view Patterns\n"
+                + "\t'4' to view Arrays\n"
+                + "\t'5' to view Strings\n"
+                + "\t'6' to view Mathematics\n"
+                + "\t'7' to view Linked Lists.\n");
     }
     
 }
