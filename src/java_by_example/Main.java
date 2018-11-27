82e249eaed2bbd4f80126a6eebe69f9f68d4c346
kadeallen123 <38875538+kadeallen123@users.noreply.github.com>
2018-11-27 20:57:25 +1300

    Clean up 1

--- src/java_by_example/Main.java
+++ src/java_by_example/Main.java
@@ -16,7 +16,7 @@ public class Main {
     // Create scanner input reference
     static Scanner input = new Scanner(System.in);
     // Variable declarations
-    static byte mode;
+    static int mode;
     static String program;
     static boolean mainLoop = true;
     public static String endStatement;
@@ -25,67 +25,16 @@ public class Main {
         new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
     }
     public static void cls() throws IOException{
-        try {
-                Main.clearConsole();
-            } 
-        catch (InterruptedException ex) {
-                Logger.getLogger(Simple.class.getName()).log(Level.SEVERE, null, ex);
-            }
+        try { Main.clearConsole(); } 
+        catch (InterruptedException ex) { Logger.getLogger(Simple.class.getName()).log(Level.SEVERE, null, ex); }
     }
-    public static void simpleAsker() throws IOException {
-        cls();
-        System.out.println("This is Simple Programs.\n"
-                    + "\t'a' to view the 'Hello World' Program\n"
-                    + "\t'b' to view the 'Two Integer Comparison' Program\n"
-                    + "\t'c' to view the 'Three Integer Comparison' Program\n"
-                    + "\t'd' to view the 'Even or Odd' Program\n"
-                    + "\t'e' to view the 'Positive or Negative' Program\n"
-                    + "\t'f' to view the 'Leap Year' Program");
-            program = input.next();
-            switch (program) {
-                case "back":
-                    categoryAsker();
-                    break;
-                case "a":
-                    Simple.helloWorld();
-                    break;
-            }
-//            if ("back".equals(program)) {
-//                categoryAsker();
-//            }
-//            if ("a".equals(program)) {
-//                Simple.helloWorld();
-//            }
-    }
-    
-    public static void categoryAsker() throws IOException{
-        cls();
-        System.out.println("Enter the correpsonding number/letter to view the corresponding category/program\n"
-                + "Enter 'back' at any time to go back to the previous selection,\n"
-                + "Enter 'exit' at any time to exit the program.\n"
-                + "\t'1' to view Simple Programs\n"
-                + "\t'2' to view Loops / Iterations\n"
-                + "\t'3' to view Patterns\n"
-                + "\t'4' to view Arrays\n"
-                + "\t'5' to view Strings\n"
-                + "\t'6' to view Mathematics\n"
-                + "\t'7' to view Linked Lists.");
-        mode = input.nextByte();
-        switch (mode) {
-            case 1:
-                simpleAsker();
-                break;
-            default:
-                categoryAsker();
-        }
-    }
-    
+        
     public static void main(String[] args) throws IOException {
        // Welcome statement
         cls();
         System.out.println("Welcome to Java By Examples\nPress 'Enter' to continue.");
         input.nextLine();
-        categoryAsker();
+        Askers.categoryAsker();
     }
     
 }
--- src/java_by_example/Simple.java
+++ src/java_by_example/Simple.java
@@ -9,26 +9,26 @@ import java.util.logging.Logger;
 
 public class Simple {
     
-    public static String endStatement;
+    private static String endStatement;
     static Scanner input = new Scanner(System.in);
     
     public static void helloWorld() throws IOException {
         Main.cls();
         System.out.println("Hello World Program!\n"
                 + "\nOutput:\n"
-                + "Hello World!\n");
-        System.out.println("Code for program:\n"
+                + "Hello World!\n"
+                + "Code for program:\n"
                 + "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n"
                 + "~ public static void main(String[] args) {\n"
                 + "~~~ System.out.println(\"Hello World!\")\n"
                 + "~ }\n"
                 + "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
         endStatement = input.nextLine();
-        if ("back".equals(endStatement) || "".equals(endStatement)) {
-            Main.cls();
-            System.out.println("");
-            Main.simpleAsker();
-        }
-        
+        Askers.simpleAsker();
+//        switch (endStatement) {
+//            default:
+//                Main.cls();
+//                Askers.simpleAsker();
+//        }
     }
 }
