1cb00ff446b4277e76f6f329871fd56612df2a87
kadeallen123 <38875538+kadeallen123@users.noreply.github.com>
2018-11-28 09:27:14 +1300

    School Changes

---
+++ Java_By_Example-master/src/java_by_example/Main.java
@@ -0,0 +1,40 @@
+/*
+    This programs displays to the user example Java programs that are categorised
+    Author: Kade Allen
+    Date of creation:   27/11/18
+*/
+
+package java_by_example;
+
+import java.util.*;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Main {
+    
+    // Create scanner input reference
+    static Scanner input = new Scanner(System.in);
+    // Variable declarations
+    static int mode;
+    static String program;
+    static boolean mainLoop = true;
+    public static String endStatement;
+    
+    public static void clearConsole() throws IOException, InterruptedException {
+        new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+    }
+    public static void cls() throws IOException{
+        try { Main.clearConsole(); } 
+        catch (InterruptedException ex) { Logger.getLogger(Simple.class.getName()).log(Level.SEVERE, null, ex); }
+    }
+        
+    public static void main(String[] args) throws IOException {
+       // Welcome statement
+        cls();
+        System.out.println("Welcome to Java By Examples\nPress 'Enter' to continue.");
+        input.nextLine();
+        Askers.categoryAsker();
+    }
+    
+}
