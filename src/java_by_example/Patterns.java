ced5434c6b7db34819bd14734967fd1b950f8f7f
kadeallen123 <38875538+kadeallen123@users.noreply.github.com>
2018-12-02 17:32:45 +1300

    Finished above max input value for floyds triangle and pascals triangle

--- src/java_by_example/Patterns.java
+++ src/java_by_example/Patterns.java
@@ -71,17 +71,28 @@ public class Patterns {
                 + "Enter number of rows to be printed (Max recommended, 13):\t");
         double rows = input.nextDouble();
         int i, j, k = 0;
+        aboveMax:
         if (rows > 13) {
             Main.cls();
             System.out.println("That value is above the maximum recommended value of 13\n"
                     + "Are you sure you would like to continue? (y\\n)");
             String cond = input.next();
             if (cond.equals("n")) {
-                System.out.println("Restarting progra");
+                Main.cls();
+                for (int q = 3; q >= 0; q--) {
+                    if (q == 0) {
+                        System.out.println("Restarting");
+                        Thread.sleep(1000);
+                        break;
+                    }
+                    System.out.printf("Restarting program in %d\n", q);
+                    Thread.sleep(1000);
+                    Main.cls();
+                }
+                floydsTriangle();
+            } else {
+                break aboveMax;
             }
-            Thread.sleep(1000);
-            Main.cls();
-            floydsTriangle();
         }
         for (i = 1; i <= rows; i++) {
             for (j = 1; j <= i; j++) {
@@ -93,7 +104,7 @@ public class Patterns {
         endStatement = input.nextLine();
         switch (input.nextLine()) {
             case "again":
-                pascalsTriangle();
+                floydsTriangle();
                 break;
             default:
                 Askers.patternsAsker();
@@ -110,6 +121,11 @@ public class Patterns {
         if (rows >= 15.0) {
             Main.cls();
             for (int i = 3; i >= 0; i--) {
+                if (i == 0) {
+                    System.out.println("Restarting");
+                    Thread.sleep(1000);
+                    break;
+                }
                 System.out.printf("That value is above the maximum of 14.\n"
                         + "Please try again in %d seconds\n", i);
                 Thread.sleep(1000);
